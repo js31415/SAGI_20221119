1206627d58f76aa4c1a9460b2aabc3aaf21c71a5
js31415 <janodevelop@gmail.com>
2022-11-20 00:04:21 +0100

    test(setup)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+    has_many :videos
+    
+    validates :name, presence: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,5 @@
 class Video < ApplicationRecord
   belongs_to :category
+
+  validates :title, presence: true
 end
