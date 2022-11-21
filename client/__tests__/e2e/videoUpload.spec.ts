75e2ce021ca82b084fb71c826ccb2fd3eb242244
js31415 <janodevelop@gmail.com>
2022-11-21 22:17:57 +0100

    test(videoUpload) wip e2e video upload

--- client/__tests__/e2e/videoUpload.spec.ts
+++ client/__tests__/e2e/videoUpload.spec.ts
@@ -3,10 +3,13 @@ import { test, expect } from '@playwright/test';
 test('Homepage has a button with upload text and navigate to the upload page.', async ({ page }) => {
   await page.goto('http://localhost:3000');
 
+
+  await expect(page.getByRole('heading')).toBeVisible() 
+
   const upload = page.getByRole('button', {name: /upload/i})
 
   await upload.click()
 
-  await expect(page).toHaveURL(/.*checkout/);
+  await expect(page).toHaveURL(/upload/);
 
 });
