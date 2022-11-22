5815704a7bef213d4f6e3e080e69f669c297ebac
js31415 <janodevelop@gmail.com>
2022-11-22 22:39:55 +0100

    test(e2e) wip add new scenario to videoUpload

--- client/__tests__/e2e/videoUpload.spec.ts
+++ client/__tests__/e2e/videoUpload.spec.ts
@@ -13,3 +13,16 @@ test('Homepage has a button with upload text and navigate to the upload page.', 
   await expect(page).toHaveURL(/upload/);
 
 });
+
+
+test.skip('Upload page has a form where you can upload your video', async ({ page }) => {
+  await page.goto('http://localhost:3000/upload');
+
+  await expect(page.getByRole('heading', {name: "Upload your video"})).toBeVisible() 
+
+  const videoTitleInput = await page.getByLabel(/video title/);
+  const videoFileInput = await page.getByLabel(/upload video/);
+  const videoCategorySelect = await page.getByLabel(/choose a catogery/)
+
+});
+
